d2832d387b749c66937d1468bf3d612cb5f58018
André Jr <andre3junior@gmail.com>
2023-08-03 21:08:38 -0300

    Update Ex 6.c

--- Lista 01/Ex 6.c
+++ Lista 01/Ex 6.c
@@ -2,9 +2,14 @@
 #include <conio.h>
 
 void main(){
-	float ava1, ava2;
+	float ava1, ava2, med;
 	
+	printf("\nEntre com a nota 1: ");
+	scanf("%f",&ava1);
+	printf("\nEntre com a nota 2: ");
+	scanf("%f",&ava2);
 	
+	med = (ava1+ava2)/2;
 	
-	
+	printf("A media semestral eh: %.2f", med);
 }
