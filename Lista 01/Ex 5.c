cbb01f4d53a080c50297519b7e3085b3388cfa94
André Jr <andre3junior@gmail.com>
2023-08-03 22:04:10 -0300

    Update Ex 5.c

--- Lista 01/Ex 5.c
+++ Lista 01/Ex 5.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <conio.h>
-
+//5.[FOR] Criar um algoritmo em que leia dez números inteiros e imprima o maior 
+//e o segundo maior número da lista.
 void main(){
-	int maior= 0
+	int maior= 0, maior2= 0, i, num=0;
 	
-	
-	
+	for (i=0;i<4;i++){
+		printf("Entre com um numero: ");
+		scanf("%d",&num);
+		
+		if (num > maior){
+			maior = num;
+		}
+		
+	}
+	printf("\nmaior: %d", maior);
+	printf("\nmaior2: %d", maior2);
 	
 }
