2e08cc1678d56467bca76df700ef7d843680b1c0
André Jr <andre3junior@gmail.com>
2023-08-03 21:00:43 -0300

    Update Ex 4 .c

--- Lista 01/Ex 4 .c
+++ Lista 01/Ex 4 .c
@@ -2,7 +2,21 @@
 #include <conio.h>
 
 void main(){
+	int totalneg=0, somapos=0, i, num;
 	
+	for(i=0;i<5;i++){
+		printf("Digite um numero: ");
+		scanf("%i",&num);
+		
+		if (num < 0){
+			totalneg++;
+		}
+		else{
+			somapos = somapos + num;	
+		};
+
+	};
 	
-	
+	printf("\nO total de num negativos sao: %i",totalneg);
+	printf("\nA soma dos num pos eh: %i", somapos);
 }
