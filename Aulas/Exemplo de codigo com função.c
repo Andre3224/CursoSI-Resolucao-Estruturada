bef9cc7c453b6d2d0d2b82b30481e457c74f23ca
André Jr <andre3junior@gmail.com>
2023-08-10 19:25:38 -0300

    .

---
+++ Aulas/Exemplo de codigo com função.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+tipo_da_funcao nome_da_funcao(tipo var1, tipo var2,... tipo varN){
+	
+	corpoda função
+	Return valor_de_retorno
+}
+Utilidade da funçao: Facilitar a leitura do codigo. Pensar no programa por pequenos blocos. Reaproveitamento de codigo.
+*/
+
+int soma(){
+	int n1, n2, soma;
+	n1= 10;
+	n2= 20;
+	soma = n1 + n2;
+	
+}
+int sub(){
+	int n1, n2, sub;
+	n1 = 20;
+	n2 = 5;
+	sub = n1 - n2;
+	
+}
+void main(){
+	soma();
+	
+}
