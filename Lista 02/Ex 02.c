26744544ff886587d27ec5f64ebb994a88fc8576
André Jr <andre3junior@gmail.com>
2023-08-10 20:20:25 -0300

    Update Ex 02.c

--- Lista 02/Ex 02.c
+++ Lista 02/Ex 02.c
@@ -5,7 +5,10 @@
 Este procedimento deverá ser chamado através do programa principal.*/
 
 void lerNome(){
-	
+	char nome[50];
+	printf("Informe seu nome: ");
+	scanf("%s", nome);
+	printf("O nome lido foi: %s", nome);
 }
 
 void main(){
