9688721679f291ee69141d86fc5d595c525048d2
André Jr <andre3junior@gmail.com>
2023-08-10 19:51:49 -0300

    Update Exemplo de codigo com função.c

--- Aulas/Exemplo de codigo com função.c
+++ Aulas/Exemplo de codigo com função.c
@@ -10,21 +10,22 @@ tipo_da_funcao nome_da_funcao(tipo var1, tipo var2,... tipo varN){
 Utilidade da funçao: Facilitar a leitura do codigo. Pensar no programa por pequenos blocos. Reaproveitamento de codigo.
 */
 
-int soma(){
+int soma_dois_numeros(){
 	int n1, n2, soma;
 	n1= 10;
 	n2= 20;
 	soma = n1 + n2;
+	printf("\nSoma: %d", soma);
 	
 }
-int sub(){
+int Subtracao(){
 	int n1, n2, sub;
 	n1 = 20;
 	n2 = 5;
 	sub = n1 - n2;
-	
+	printf("\nSubtracao: %d", sub);
 }
 void main(){
-	soma();
-	sub();
+	soma_dois_numeros();
+	Subtracao();
 }
