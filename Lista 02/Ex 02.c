8530a4c9085a538435986d3df918f862613520bf
André Jr <andre3junior@gmail.com>
2023-08-10 20:15:29 -0300

    Update Ex 02.c

--- Lista 02/Ex 02.c
+++ Lista 02/Ex 02.c
@@ -4,13 +4,10 @@
 /*Escreva um procedimento que leia seu nome e após a leitura e exiba o nome lido. 
 Este procedimento deverá ser chamado através do programa principal.*/
 
-void lernome(){
-	char nome;
-	printf("Digite o seu nome: ");
-	scanf("%c",&nome);
-	printf("O nome lido foi: %d", nome);
+void lerNome(){
+	
 }
 
 void main(){
-	lernome();
+	lerNome();
 }
