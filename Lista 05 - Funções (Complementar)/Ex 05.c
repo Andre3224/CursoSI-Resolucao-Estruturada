6d22f8a5ab13f66584082cfc903952c553a08335
André Jr <andre3junior@gmail.com>
2023-08-11 17:21:23 -0300

    .

--- Lista 05 - Funções (Complementar)/Ex 05.c
+++ Lista 05 - Funções (Complementar)/Ex 05.c
@@ -2,3 +2,18 @@
 #include <conio.h>
 #include <stdlib.h>
 
+float VolEsfera(){
+	float PI=3.1415, raio, resp;
+	printf("Digite o valor do Raio: ");
+	scanf("%f",&raio);
+	
+	resp = (4.0/3.0)*PI*(raio*raio*raio);
+	return resp;
+	
+}
+
+void main(){
+	float retorno;
+	retorno = VolEsfera();
+	printf("O retorno eh: %.2f", retorno);
+}
