1246a8b0256b781d04e98acc6f35e3967de1c957
André Jr <andre3junior@gmail.com>
2023-08-17 22:11:16 -0300

    .

--- Lista 06 - Passagem de parametros por valor/Ex 09.c
+++ Lista 06 - Passagem de parametros por valor/Ex 09.c
@@ -1,2 +1,29 @@
 #include<stdio.h>
 #include<conio.h>
+
+float PesIdeal(float alt){
+	float pes;
+	int op;
+	printf("Voce e Homem (1) ou Mulher (2): ");
+	scanf("%d", &op);
+	
+	if(op == 1){
+		pes = 72.7;
+		return pes;
+	}else if(op == 2){
+		pes = 44.7;
+		return pes;
+	}else{
+		printf("Opcao invalida...");
+	}
+}
+
+void main(){
+	float alt;
+	
+	printf("Digite a altura: ");
+	scanf("%f", &alt);
+	
+	
+	printf("O peso ideal eh: %f", PesIdeal(alt));
+}
