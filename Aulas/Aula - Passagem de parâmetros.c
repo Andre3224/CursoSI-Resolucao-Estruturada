9b4e714ada8ed897a74163479879300167f407ec
André Jr <andre3junior@gmail.com>
2023-08-17 21:09:52 -0300

    .

--- Aulas/Aula - Passagem de parâmetros.c
+++ Aulas/Aula - Passagem de parâmetros.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <conio.h>
 
-int somar(int num, int num2){ //declaração de parametro (mesma funçao da variavel)
+int somar(int num, int num2){ //declaração de parametro (mesma funçao da variavel) -> Passagem por valor
 	int soma;
 	soma = num + num2;
 	return soma;
 }
-
+int mult(int *num, int *num2){
+	int prod;
+	*num = *num + 2;
+	prod = *num * *num2;	
+	return prod;
+}
 void main(){
 	int num, num2;
 	//A passagem de parametro eh enviar os valores de uma variavel para outro lugar
@@ -15,7 +20,9 @@ void main(){
 	scanf("%d",&num);
 	printf("\nDigite o n2: ");
 	scanf("%d",&num2);
-	
-	printf("\nA soma eh: %d", somar(num, num2)); //"Teletransportar" valor da variavel
-	
+	printf("Antes da alteraao \nnum = %d \n num2 = %d", num, num2);
+	printf("\nA soma eh: %d", somar(num, num2)); //"Teletransportar" COPIA valor da variavel para o modulo somar, o conteudo original é mantido
+	//Passagem por referencia
+	printf("\nO produto eh: %d", mult(&num, &num2)); //Para enviar por referencia é preciso enviar com &
+	printf("\n Apos a passagem: num1 = %d", num); //passagem por Referencia
 }
---
+++ Lista 06 - Passagem de parametros por valor/Ex 01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <conio.h>
+/*1.	Escreva uma função, que receba por parâmetro dois valores A e B, calcule e retorne a soma dos valores.*/
+
+void main(){
+	
+}
